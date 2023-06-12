01a55d45f714d1f2ec89a69507fb65165ac31657
Sadia Binta Kabir <sadiaa.kabir@gmail.com>
2023-06-12 10:51:03 -0400

    admin role added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const app= express();
 const port=process.env.PORT || 5000;
 
@@ -32,6 +33,10 @@ async function run() {
     const userCollection=client.db("summerSchoolDb").collection("users");
 
     //user
+    app.get('/users',async(req,res)=>{
+      const result=await userCollection.find().toArray();
+      res.send(result);
+    })
     app.post('/users',async(req,res)=>{
       const user=req.body;
       const query={email:user.email};
@@ -41,7 +46,19 @@ async function run() {
       }
       const result=await userCollection.insertOne(user);
       res.send(result);
+    });
+    app.patch('/users/admin/:id', async(req,res)=>{
+      const id=req.params.id;
+      const filter ={_id: new ObjectId(id)};
+      const updateDoc={
+        $set:{
+          role:'admin'
+        },
+      };
+      const result=await userCollection.updateOne(filter,updateDoc);
+      res.send(result);
     })
+
 //class
     app.get('/classes',async(req,res)=>{
       const result=await classCollection.find().toArray();
